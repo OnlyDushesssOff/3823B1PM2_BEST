1bbaf7ecbd5f0c4a80927e0f6a45c3d9978ac258
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2023-10-23 00:01:19 +0300

    New comment

--- 2-if_else_switch/Chocolate/chocolate.h
+++ 2-if_else_switch/Chocolate/chocolate.h
@@ -1,3 +1,10 @@
 int chocolate(int n, int m, int k){
-    return 0;
+    if ((n <= 0) || (m <= 0))
+        return 0;
+    else if (n * m == k)
+        return 1;
+    else if ((k % n == 0) || (k % m == 0))
+        return 1;
+    else
+        return 0;
 }
