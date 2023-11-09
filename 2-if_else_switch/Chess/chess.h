9ac938c97d30d9fca39b23beae7361d0734e4e70
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2023-11-09 18:04:42 +0300

    123dfdf

--- 2-if_else_switch/Chess/chess.h
+++ 2-if_else_switch/Chess/chess.h
@@ -1,10 +1,3 @@
-int chocolate(int n, int m, int k){
-    if ((n <= 0) || (m <= 0))
-        return 0;
-    else if (n * m == k)
-        return 1;
-    else if ((k % n == 0) || (k % m == 0))
-        return 1;
-    else
-        return 0;
+int chess_rook(char a, int b, char c, int d){
+    return 0;
 }
