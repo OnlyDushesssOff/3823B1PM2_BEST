a242f6303b364810efbf4906569c6a1dc3f07cba
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2023-11-13 19:44:38 +0300

    add

--- 2-if_else_switch/Chess/chess.h
+++ 2-if_else_switch/Chess/chess.h
@@ -1,7 +1,13 @@
-<<<<<<< HEAD
 int chess_rook(char a, int b, char c, int d) {
-=======
-int chess_rook(char a, int b, char c, int d){
->>>>>>> 7226bcd6d18eb537e35df2a8f69e8e4b7d4e086b
-    return 0;
+    int bukva1 = a, chislo1 = b, bukva2 = c, chislo2 = d;
+    if ((bukva1 <= 64) || (bukva1 >= 73) || (bukva2 <= 64) || (bukva2 >= 73) || (chislo1 >= 9) || (chislo2 >= 9))
+        return 0;
+    else if ((bukva1 == bukva2) && (chislo1 == chislo2))
+        return 0;
+    else if (bukva1 == bukva2)
+        return 1;
+    else if (chislo1 == chislo2)
+        return 1;
+    else
+        return 0;
 }
