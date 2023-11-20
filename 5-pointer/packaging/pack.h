30f3d1934a7af004f800143990a46f39c0d95b0a
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2023-11-20 13:15:24 +0300

    New points

--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,9 +1,15 @@
 #include <math.h>
+#include <stdlib.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-   void* ind = arr;
+    unsigned long long* mem = arr;
+    if (size > 8) {
+        return 0;
+    }
+    return mem[0];
 }
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
+    unsigned char* bait = input;
+    return bait[4];
 }
