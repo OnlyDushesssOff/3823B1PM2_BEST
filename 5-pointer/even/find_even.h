040ed68e946d5ffb2e10ae81dd06096acf716376
OnlyDushesssOff <andreyrabotaem825@gmail.com>
2023-11-26 16:13:39 +0300

    dfsaf

--- 5-pointer/even/find_even.h
+++ 5-pointer/even/find_even.h
@@ -1,13 +1,11 @@
 #include <math.h>
+#include <stddef.h>
 
 int* find_even(int* arr, int size){
     int prov = 1;
     for (int i = 0; i < size; i++) {
         if (arr[i] % 2 == 0) {
-<<<<<<< HEAD
             prov = 0;
-=======
->>>>>>> fc3c644f35690a2386309d72d14f05c989666fe4
             int* ans = &arr[i];
             return ans;
             break;
